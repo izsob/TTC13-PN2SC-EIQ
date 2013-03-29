40b7e4464233e9a46f8ba1393ddac4982eea4c51
ttc13-pn2sc-eiq-share <ubuntu@ubuntu-VirtualBox>
2013-03-29 21:17:24 +0100

    ttc share version, update scripts

--- transformation/pn2sc.native/run.sh
+++ transformation/pn2sc.native/run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-path="/home/benedek/data/research/ttc/runtime-Instances/instances"
+# usage:
+# 1st param is the input model without extension inside "path" (see below). 
+# ./run.sh testcases/testcase1-in
 
-sourceFile="testcases/testcase10-in.petrinet"
-targetFile="testcases/testcase10-out.statecharts"
-product/pn2sc/eclipse \
-  -basePath $path -sourceFile $sourceFile -targetFile $targetFile \
+path="/home/ubuntu/ttc13/git/TTC13-PN2SC-EIQ/transformation/instances"
+sourceFile=$1
+
+# revert instance model changes
+echo "Clean old changes"
+cd $path
+git checkout ${sourceFile}.petrinet 2> /dev/null
+rm ${path}/${sourceFile}.statecharts 2> /dev/null
+rm ${path}/${sourceFile}.pn2sctrace 2> /dev/null
+
+echo "Run transformation benchmark"
+echo
+# run transformation
+/home/ubuntu/ttc13/git/TTC13-PN2SC-EIQ/transformation/pn2sc.native/product/pn2sc/eclipse \
+  -basePath $path -sourceFile ${sourceFile} \
   -debugTransform 0 -changeDriven 0
+
+echo
+echo "Output is saved to ${path}/${sourceFile}.[petrinet | statecharts | pn2sctrace]"
+echo
+read -p "Press [ENTER] key to close."
--- transformation/pn2sc.native/runPBM.sh
+++ transformation/pn2sc.native/runPBM.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-path="/home/benedek/data/research/ttc/runtime-Instances/instances/performance"
+path="/home/ubuntu/ttc13/git/TTC13-PN2SC-EIQ/transformation/instances/performance"
 cd $path
 
+/home/ubuntu/ttc13/git/TTC13-PN2SC-EIQ/transformation/pn2sc.native/cleanDir.sh $path
+
 for i in `ls -1rS *.petrinet`; do
-sourceFile=${i}
-targetFile=`basename ${i} .petrinet`.statecharts
-/home/benedek/data/research/ttc/runtime-Instances/pn2sc.native/product/pn2sc/eclipse \
-  -basePath $path -sourceFile $sourceFile -targetFile $targetFile \
+sourceFile=`basename ${i} .petrinet`
+/home/ubuntu/ttc13/git/TTC13-PN2SC-EIQ/transformation/pn2sc.native/product/pn2sc/eclipse \
+  -basePath $path -sourceFile $sourceFile \
   -debugTransform 0 -changeDriven 0
 done
+
+read -p "Press [ENTER] key to close."
